e84d4d610c129574df7b5e703ae63368d5bff1ac
Noah Huppert <contact@noahh.io>
2018-10-21 21:00:09 -0400

    omega-relay: wrote simple port on off loop

--- devices/omega-relay/src/main.rs
+++ devices/omega-relay/src/main.rs
@@ -1,18 +1,38 @@
 extern crate byteorder;
 
+use std::time::Duration;
+use std::thread;
+
 pub mod gpio;
 
 fn main() {
     let port1 = gpio::GPIOPort{number: 1u8};
 
-    // Set value
-    println!("===== Set GPIO value to 1");
-    
-    let value = port1.get_value().unwrap();
-    println!("Before: {:?}", value);
+    port1.set_status(gpio::GPIOPortStatus::Exported)
+        .expect("error exporting port");
 
-    port1.set_value(true).unwrap();
+    port1.set_direction(gpio::GPIOPortDirection::Out)
+        .expect("error setting port to out direction");
 
-    let value = port1.get_value().unwrap();
-    println!("After: {:?}", value);
+    let mut port_v = true;
+
+    for _ in 0..10 {
+        match port1.set_value(port_v) {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("error setting port value to: {}, error: {}",
+                          port_v, e);
+                break;
+            }
+        }
+
+        println!("set port to: {}", port_v);
+
+        port_v = !port_v;
+
+        thread::sleep(Duration::from_millis(1000));
+    }
+
+    port1.set_status(gpio::GPIOPortStatus::Unexported)
+        .expect("error unexporting port");
 }
